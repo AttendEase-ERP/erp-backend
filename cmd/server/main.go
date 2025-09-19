15c52eef48b121d9502440ba1a81993b0e64cd43
Yaten <yaten598@gmail.com>
2025-09-19 16:42:01 +0530

    add clerk authentication

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,43 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/AttendEase-ERP/erp-backend/internal/auth"
 	"github.com/AttendEase-ERP/erp-backend/pkg/config"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func main() {
+	// initialize resources
 	cfg := config.LoadConfig()
+	auth.InitClerk(cfg.ClerkSecretKey)
+	r := chi.NewRouter()
 
-	fmt.Println(cfg.DatabaseURL)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		// MaxAge:           300,
+	}))
+	r.Use(auth.RequireAuth)
+
+	r.Get("/auth-check", authHandlerFunc)
+
+	http.ListenAndServe(cfg.Port, r)
+}
+
+func authHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	claims, err := auth.GetUserEmail(r.Context())
+	if err != nil {
+		fmt.Println(claims)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println(claims)
+	w.Write([]byte(claims))
 }
---
+++ internal/auth/auth.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+	clerkhttp "github.com/clerk/clerk-sdk-go/v2/http"
+	"github.com/clerk/clerk-sdk-go/v2/user"
+)
+
+func InitClerk(secret string) {
+	clerk.SetKey(secret)
+}
+
+func RequireAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler := clerkhttp.WithHeaderAuthorization()(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, ok := clerk.SessionClaimsFromContext(r.Context())
+			// usr, err := user.Get(r.Context(), claims.Subject)
+
+			if !ok {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		}))
+
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// Extract session claims
+func GetSessionClaimsFromContext(ctx context.Context) (*clerk.SessionClaims, error) {
+	claims, ok := clerk.SessionClaimsFromContext(ctx)
+	if !ok {
+		return nil, errors.New("no session claims in context")
+	}
+
+	return claims, nil
+}
+
+// Extract user email from session claim
+func GetUserEmail(ctx context.Context) (string, error) {
+	claims, err := GetSessionClaimsFromContext(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	usr, err := user.Get(ctx, claims.Subject)
+	if err != nil {
+		return "", errors.New("no user found")
+	}
+
+	return usr.EmailAddresses[0].EmailAddress, nil
+}

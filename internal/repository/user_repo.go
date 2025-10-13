225b82c8df2b5d59dd1eb2cba8cfacf15e587569
Yaten <yaten598@gmail.com>
2025-10-13 16:48:27 +0530

    update role fetching in GetUserByEmail()

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/AttendEase-ERP/erp-backend/internal/db"
 	"github.com/AttendEase-ERP/erp-backend/internal/models"
+	"github.com/AttendEase-ERP/erp-backend/pkg/logger"
 )
 
 func GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
-	row := db.Pool.QueryRow(ctx, `SELECT id, email, role FROM "Users" WHERE "email" ILIKE '%$1%'`, email)
+	row := db.Pool.QueryRow(ctx, `SELECT id, email, role FROM "Users" WHERE "email" ILIKE $1`, "%"+email+"%")
+
 	var u models.User
 	if err := row.Scan(&u.ID, &u.Email, &u.Role); err != nil {
+		logger.Log.Error().Err(err).Msg(err.Error())
 		return nil, err
 	}
 

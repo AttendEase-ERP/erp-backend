48f4ad34a2e712f84a10d694230131365ecd5185
Yaten <yaten598@gmail.com>
2026-01-12 07:26:44 +0000

    remove commented code
    Signed-off-by: Yaten <yaten598@gmail.com>

--- internal/models/users.go
+++ internal/models/users.go
@@ -15,44 +15,7 @@ type UserDetails struct {
 	CourseDuration   string   `json:"course_duration"`
 	EnrollmentNumber string   `json:"enrollment_number"`
 }
-/* 
-type AttendanceStatus string
 
-const (
-	AttendancePresent AttendanceStatus = "present"
-	AttendanceAbsent  AttendanceStatus = "absent"
-)
-
-type Student struct {
-	ID               string `db:"id"`
-	Name             string `db:"name"`
-	Email            string `db:"email"`
-	EnrollmentNumber string `db:"enrollment_number"`
-	CurrentSemester  int    `db:"current_semester"`
-	SectionID        string `db:"section_id"`
-}
-
-type Section struct {
-	ID          string `db:"id"`
-	SectionName string `db:"section_name"`
-	CourseID    string `db:"course_id"`
-}
-
-type Course struct {
-	ID             string `db:"id"`
-	CourseName     string `db:"course_name"`
-	CourseDuration string `db:"course_duration"`
-}
-
-type Attendance struct {
-	ID        string           `db:"id"`
-	StudentID string           `db:"student_id"`
-	TeacherID string           `db:"teacher_id"`
-	SubjectID string           `db:"subject_id"`
-	Date      time.Time        `db:"date"`
-	Status    AttendanceStatus `db:"attendance_status"`
-}
- */
 type StudentsAttendanceList struct {
 	ID               string `json:"id"`
 	Name             string `json:"name"`
